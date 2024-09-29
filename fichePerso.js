76996f6539b9c2beaf46ada15d8fd9a5686f7335
Jules Finck <55241502+copsfuraxx@users.noreply.github.com>
2024-09-29 13:30:10 +0200

    feat: add stats

--- fichePerso.js
+++ fichePerso.js
@@ -1,3 +1,5 @@
+let data;
+
 function ReadFile() {
     const fileElement = document.getElementById("file");
     const reader = new FileReader();
@@ -12,9 +14,19 @@ function ReadFile() {
 
 function LoadData(file) {
     const persoElement = document.getElementById("perso");
-    const data = JSON.parse(file);
-    for (const [key, value] of Object.entries(data)) {
+    data = JSON.parse(file);
+    for (const [key, value] of Object.entries(data.resume)) {
         document.getElementById(key).innerText = value;
-      }
+    }
+    let parentElement = document.getElementById("ability");
+    for (const [key, value] of Object.entries(data.ability)) {
+        const cells = parentElement.querySelector("#ability-" + key).cells;
+        cells[1].innerText = value;
+        cells[2].innerText = "+" + (Math.floor(value / 2) - 5);
+    }
     persoElement.classList.remove("hidden");
+}
+
+function print(text) {
+    console.log(text);
 }
